37b2365c02f848c1b1fbe627ff61f09f0cdb602a
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2016-06-04 02:29:43 -0400

    changed carousel captions, added subcaptions, added location and map

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 $(function() {
   "use strict";
 
-  var wheight = $(window).height() - $('.navbar-fixed-top').height() - $('.navbar-fixed-bottom').height() - $('.description').outerHeight() - 15; //get height of window
-  $('.fullheight').css('height', wheight); //set .fullheight to window size
+  var wheight = $(window).height(); //get height of window
+  var wwidth = $(window).width(); //get width of window
+  var wcarousel = wheight - $('.navbar-fixed-top').height() - $('.navbar-fixed-bottom').height() - $('.description').outerHeight() - 15; //get height of carousel
+  $('.fullheight').css('height', wcarousel); //set .fullheight to window size
+
+
+  $('.carousel-caption p').each(function(){
+    if(wwidth < 768){
+      $(this).hide();
+    } else {
+      $(this).show();
+    }
+  });
+
+  // $('#map').each(function(){
+  //   if(wwidth < 768){
+  //     $(this).addClass('text-left');
+  //   } else {
+  //     $(this).addClass('text-right');
+  //   }
+  // });
 
   //replace img inside carousels with a background image
   $('#carousel-display .item img').each(function(){
@@ -14,7 +33,18 @@ $(function() {
   //adjust height of .fullheight elements on window resize
 
   $(window).resize(function(){
-    var wheight = $(window).height() - $('.navbar-fixed-top').height() - $('.navbar-fixed-bottom').height() - $('.description').outerHeight() - 15; //get height of window
-    $('.fullheight').css('height', wheight); //set .fullheight to window size
+    var wheight = $(window).height(); //get height of window
+    var wcarousel = wheight - $('.navbar-fixed-top').height() - $('.navbar-fixed-bottom').height() - $('.description').outerHeight() - 15; //get height of carousel
+    $('.fullheight').css('height', wcarousel); //set .fullheight to window size
+    var wwidth = $(window).width();
+    if(wwidth < 768){
+      $('.carousel-caption p').hide();
+      // $('#map').addClass('text-left');
+      // $('#map').removeClass('text-right');
+    } else {
+      $('.carousel-caption p').show();
+      // $('#map').addClass('text-right');
+      // $('#map').removeClass('text-left');
+    }
   });
 });
