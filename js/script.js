9a87026cd7eaaa04601610fa8e93de535d0af6bb
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2016-06-06 18:48:34 -0400

    made carousel fade, added prefixing library, smoothened scrolling,

--- js/script.js
+++ js/script.js
@@ -6,22 +6,46 @@ $(function() {
   var wcarousel = wheight - $('.navbar-fixed-top').height() - $('.navbar-fixed-bottom').height() - $('.description').outerHeight() - 15; //get height of carousel
   $('.fullheight').css('height', wcarousel); //set .fullheight to window size
 
-    if(wwidth < 768){
-      $('.carousel-caption p').hide();
-    } else {
-      $('.carousel-caption p').show();
-    }
+  var topoffset = 50; //variable for menu height
+
+  //Activate Scrollspy
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: topoffset
+  });
+
+  // add inbody class
+  var hash = $(this).find('li.active a').attr('href');
+  if(hash !== '#page-top') {
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+
+  //show carousel caption only on sizes bigger than xs
+  if(wwidth < 768){
+    $('.carousel-caption p').hide();
+  } else {
+    $('.carousel-caption p').show();
+  }
 
 
 
-
-  // $('#map').each(function(){
-  //   if(wwidth < 768){
-  //     $(this).addClass('text-left');
-  //   } else {
-  //     $(this).addClass('text-right');
-  //   }
-  // });
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') ===
+      this.pathname.replace(/^\//,'') &&
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
 
   //replace img inside carousels with a background image
   $('#carousel-display .item img').each(function(){
@@ -31,7 +55,6 @@ $(function() {
   });
 
   //adjust height of .fullheight elements on window resize
-
   $(window).resize(function(){
     var wheight = $(window).height(); //get height of window
     var wcarousel = wheight - $('.navbar-fixed-top').height() - $('.navbar-fixed-bottom').height() - $('.description').outerHeight() - 15; //get height of carousel
@@ -39,12 +62,8 @@ $(function() {
     var wwidth = $(window).width();
     if(wwidth < 768){
       $('.carousel-caption p').hide();
-      // $('#map').addClass('text-left');
-      // $('#map').removeClass('text-right');
     } else {
       $('.carousel-caption p').show();
-      // $('#map').addClass('text-right');
-      // $('#map').removeClass('text-left');
     }
   });
 });
